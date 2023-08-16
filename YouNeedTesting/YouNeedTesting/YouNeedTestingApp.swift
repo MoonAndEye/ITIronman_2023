490a5eb2fa957f0330c17142f7625a2dec66441b
MarvinLin <atimis19@gmail.com>
2023-08-16 17:23:02 +0800

    init: project init

---
+++ YouNeedTesting/YouNeedTesting/YouNeedTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouNeedTestingApp.swift
+//  YouNeedTesting
+//
+//  Created by cm0679 on 2023/8/16.
+//
+
+import SwiftUI
+
+@main
+struct YouNeedTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
