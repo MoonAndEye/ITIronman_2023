17c4a27064f6aaf7ef224d6db00dc7a5bd0fd9c0
MarvinLin <atimis19@gmail.com>
2023-08-30 17:51:14 +0800

    project init: 開 TwSTockTools 寫台股小工具與 Unit testing

---
+++ TwStockTools/TwStockTools/TwStockToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwStockToolsApp.swift
+//  TwStockTools
+//
+//  Created by cm0679 on 2023/8/30.
+//
+
+import SwiftUI
+
+@main
+struct TwStockToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
