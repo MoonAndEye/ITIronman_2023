03b266310106cfd0d895ed5e550306e1503b73a4
MarvinLin <atimis19@gmail.com>
2023-09-05 23:47:32 +0800

    add: file DateUtility and testing

---
+++ TwStockTools/TwStockTools/Utilities/DateUtility.swift
@@ -0,0 +1,13 @@
+//
+//  DateUtility.swift
+//  TwStockTools
+//
+//  Created by cm0679 on 2023/9/5.
+//
+
+import Foundation
+
+struct DateUtility {
+    
+    
+}
---
+++ TwStockTools/TwStockToolsTests/DateUtilityTests.swift
@@ -0,0 +1,20 @@
+//
+//  DateUtilityTests.swift
+//  TwStockToolsTests
+//
+//  Created by cm0679 on 2023/9/5.
+//
+
+import XCTest
+@testable import TwStockTools
+
+final class DateUtilityTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+}
