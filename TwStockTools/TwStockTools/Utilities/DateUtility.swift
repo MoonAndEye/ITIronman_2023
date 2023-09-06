d454c8135eec8d4487396b63840b4a65b64ca2e4
MarvinLin <atimis19@gmail.com>
2023-09-06 09:59:10 +0800

    test: DateUtility 實作與測試

--- TwStockTools/TwStockTools/Utilities/DateUtility.swift
+++ TwStockTools/TwStockTools/Utilities/DateUtility.swift
@@ -9,5 +9,13 @@ import Foundation
 
 struct DateUtility {
     
+    static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        return dateFormatter
+    }()
     
+    func getString(from date: Date) -> String {
+        return DateUtility.dateFormatter.string(from: date)
+    }
 }
--- TwStockTools/TwStockToolsTests/DateUtilityTests.swift
+++ TwStockTools/TwStockToolsTests/DateUtilityTests.swift
@@ -16,5 +16,34 @@ final class DateUtilityTests: XCTestCase {
 
     override func tearDownWithError() throws {
     }
+    
+    /// 2023-09-05 05:09:55
+    private var timeInterval: TimeInterval {
+        1693890595
+    }
+    /// 2023-09-05 05:09:55
+    private var date: Date {
+        Date(timeIntervalSince1970: timeInterval)
+    }
+    
+    /// 2023-09-06 01:43:45
+    private var timeIntervale2: TimeInterval {
+        1693964625
+    }
+    
+    private var date2: Date {
+        Date(timeIntervalSince1970: timeIntervale2)
+    }
 
+    func testDateToString() {
+        
+        let sut = DateUtility()
+        let string = sut.getString(from: date)
+        let answer = "2023-09-05"
+        XCTAssertEqual(string, answer)
+        
+        let string2 = sut.getString(from: date2)
+        let answer2 = "2023-09-06"
+        XCTAssertEqual(string2, answer2)
+    }
 }
